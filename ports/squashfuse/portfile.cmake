308127c32b0eb6e96cf1c104739c66c951ba4f6e
Håvard Bjerke <h0wy36@gmail.com>
2025-10-01 03:47:23 +0200

    ports/squashfuse: Build fixes...

--- ports/squashfuse/portfile.cmake
+++ ports/squashfuse/portfile.cmake
@@ -2,13 +2,13 @@ vcpkg_download_distfile(ARCHIVE
     URLS https://github.com/vasi/squashfuse/archive/0.5.2.tar.gz
     FILENAME squashfuse.tar.gz
     SHA512 4380d73afa47105e2188a4eebc6b54c5961a2ba38e5986865b7fd762ed0aea7d24b0d9f5b674abd9c29f73a336bd69142db7869a548560723f60559859cdd7f3
-    PATCHES
-        include-fixes.patch
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
+    PATCHES
+        include-fixes.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
